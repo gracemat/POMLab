42e0216494b9400540af8cda607d068dff6b2f61
realgreeshma <greeshmahere@gmail.com>
2019-08-12 20:00:53 -0500

    Changed some values as account details got changed

--- src/main/java/com/qa/activecampaign/constants/ActiveCampaign_ConstantValues.java
+++ src/main/java/com/qa/activecampaign/constants/ActiveCampaign_ConstantValues.java
@@ -3,12 +3,12 @@ package com.qa.activecampaign.constants;
 public class ActiveCampaign_ConstantValues {
 	public static final String  ACTIVECAMPAIGN_LOGINPAGE_TITLE = "Login To Your Email Marketing Account";
 	
-	public static final String ADMINPAGE_TITLE ="GRACEMATHAM Email Marketing";
+	public static final String ADMINPAGE_TITLE ="GREESHMAHERE Email Marketing";
 	
-	public static final String OVERVIEW_PAGE_TITLE = "GRACEMATHAM Email Marketing - Overview";
+	public static final String OVERVIEW_PAGE_TITLE = "GREESHMAHERE Email Marketing - Overview";
 	
-	public static final String OVERVIEW_PAGE_URL = "https://gracematham.activehosted.com/overview/";
+	public static final String OVERVIEW_PAGE_URL = "https://greeshmahere.activehosted.com/overview/";
 	
-	public static final String USERNAME = "Grace";
+	public static final String USERNAME = "TestAutomation";
 	
 }
